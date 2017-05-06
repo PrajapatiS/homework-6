9b476c741443d3e78b781da76bd1f62be49e9fec
Sumanlata Prajapati <sumanlataprajapati@Sumanlatas-MacBook-Pro.local>
2017-05-06 15:44:43 -0400

    Oregon Trail weekday assignment

--- hwork.js
+++ hwork.js
@@ -19,7 +19,7 @@ function makeWagon(capacity){
 
     return{
             capicity: capacity,
-            passager: [],
+            passenger: [],
     };
 }
 
@@ -54,33 +54,48 @@ console.log(Math.random());
 
 
 //join wegon,Traveler
-join(wegon, traveler);
-join(wegon,traveler2)
 
+let wagon= makeWagon(1);
+console.log(wagon);
+ function join(wagon, traveler){
+     if(wagon.capacity=== traveler.length){
+         wagon.passenger.push(traveler);
+     }else{
+         console.log('no capicity');
+     }
 
-
-///food(wegon)
-
-function food(wegon){
-
-      let total=0;
-      total=total+wegon;
-      console.log(total);
-
-    return total;
-}
-
+ }
+console.log(join(wagon,person))
+ console.log(wagon);
 /// quaratine(wegon)
 
-let unhealthy=0;
-function quaratine(person){
-    if(person.unhealthy===0){
-        return true;
+
+function quaratine(wagon){
+    let Ishealthy=0;
+    
+    for(let i=0; i<wagon.passenger.length; i++){
+    if(wagon.passenger[i].Ishealthy===0){
+        Ishealthy=true;
         console.log('person healthy');
     }else{
-        return false;
-        console.log('All healthy');
+        Ishealthy= false;
+        console.log('peeson not healthy');
     }
-    
 }
+   return Ishealthy;
+}
+console.log(quaratine(wagon));
+///food(wegon)
+
+function food(wagon){
+    let total=0;
+   for(let i=0; i<wagon.passenger.length; i++) {
+
+      total=total+wagon.passenger[i].amount;
+   }
+   return total;
+   
+}
+console.log('total:' +food(wagon));
+
 
